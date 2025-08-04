9e1bb6fb6e884a4bbc7e1731c56b7da90f87cb99
Varshithatangeti <tangeti2005@gmail.com>
2025-08-04 20:43:32 +0530

    Vibe snehith

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import DisplaySearchItems from '@/components/DisplaySearchItems'
 import Navbar from '@/components/Navbar'
-import React from 'react'
+import React, {Suspense} from 'react'
 
 const HomePage = () => {
   return (
     <div className='w-dvw @container h-dvh flex items-center bg-background justify-center'>
       <div className='sm:w-4xl overflow-auto h-11/12 max-sm:w-full max-sm:h-full border shadow-2xl dark:bg-foreground/5 rounded-md'>
       <Navbar />
+      <Suspense fallback = {<div>Loading...</div>}>
       <DisplaySearchItems />
+      </Suspense>
       </div>
     </div>
   )
